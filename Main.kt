819c3126564db4c87544b656ffa001f65e1c2916
Dmitry <slomov.dima@mail.ru>
2024-10-30 19:53:41 +0300

    Добавлен: CHECK CalculateBMI

--- Main.kt
+++ Main.kt
@@ -4,7 +4,17 @@ import Codewars.kyu8.*
 fun main() {
 
     // CHECK repeatStr
+    println("CHECK repeatStr")
     println(StringRepeat.repeatStr(5,"Hi!"))
+
     // CHECK FixStringCase
+    println("\nCHECK FixStringCase")
     println(FixStringCase.solve("HELlo"))
+
+    // CHECK CalculateBMI
+    println("\nCHECK CalculateBMI")
+    println(CalculateBMI.bmi(55.0, 1.75))
+
+
+
 }
