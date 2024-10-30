4aaac181e8e3187c502515123985b0385d0e82b4
Dmitry <slomov.dima@mail.ru>
2024-10-30 19:54:45 +0300

    Solve Codewars kyu8 task: Calculate BMI

---
+++ Codewars/kyu8/CalculateBMI.kt
@@ -0,0 +1,33 @@
+/*
+Write function bmi that calculates body mass index (bmi = weight / height^2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+*/
+
+
+package Codewars.kyu8
+
+object CalculateBMI {
+
+    fun bmi(weight: Double, height: Double): String {
+
+        val bmi = weight / (height*height)
+
+        return when {
+            bmi > 30.0 -> "Obese"
+            bmi > 25.0 -> "Overweight"
+            bmi > 18.5 -> "Normal"
+            else -> "Underweight"
+        }
+
+    }
+
+}
+
+// Done
