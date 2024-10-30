48a168077fd91bac3762c05037e431bebbba7466
Dmitry <slomov.dima@mail.ru>
2024-10-30 20:29:57 +0300

    Добавлен статус

--- Main.kt
+++ Main.kt
@@ -3,27 +3,27 @@ import Codewars.kyu8.*
 
 fun main() {
 
-    // CHECK repeatStr
-    println("CHECK repeatStr")
+    // CHECK repeatStr - Done
+    println("CHECK repeatStr:")
     println(StringRepeat.repeatStr(5,"Hi!"))
 
-    // CHECK FixStringCase
-    println("\nCHECK FixStringCase")
+    // CHECK FixStringCase - Done
+    println("\nCHECK FixStringCase:")
     println(FixStringCase.solve("HELlo"))
 
-    // CHECK CalculateBMI
-    println("\nCHECK CalculateBMI")
+    // CHECK CalculateBMI - Done
+    println("\nCHECK CalculateBMI:")
     println(CalculateBMI.bmi(55.0, 1.75))
 
-    // CHECK EvenOrOdd
-    println("\nEvenOrOdd")
+    // CHECK EvenOrOdd - Done
+    println("\nEvenOrOdd:")
     val oddNum = 15
     val evenNum = 20
     println("$oddNum is ${EvenOrOdd.evenOrOdd(oddNum)}") // Odd
     println("$evenNum is ${EvenOrOdd.evenOrOdd(evenNum)}") // Even
 
-    // CHECK VowelCount
-    println("\nVowelCount")
+    // CHECK VowelCount - Done
+    println("\nVowelCount:")
     println(VowelCount.getCount("aaaagrgtgre"))
 
 
