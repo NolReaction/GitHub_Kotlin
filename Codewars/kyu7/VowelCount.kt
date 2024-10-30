7f78cc1158ddf5a200ba024ba31aaeb5c0966832
Dmitry <slomov.dima@mail.ru>
2024-10-30 20:28:12 +0300

    Solve Codewars kyu7 task: Vowel Count

---
+++ Codewars/kyu7/VowelCount.kt
@@ -0,0 +1,29 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+ */
+
+
+package Codewars.kyu7
+
+object VowelCount {
+
+    fun getCount(str: String): Int {
+        val vowels = "aeiou"
+        var counter = 0
+
+        for (char in str) {
+            if (char in vowels) {
+                counter++
+            }
+        }
+
+        return counter
+    }
+
+}
+
+// Done
