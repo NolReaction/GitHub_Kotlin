afaf495d7f6d633730af8ce99c89a24f28ffaf36
Dmitry <slomov.dima@mail.ru>
2024-11-13 21:55:15 +0300

    Done

--- Codewars/kyu8/SumOfPositive.kt
+++ Codewars/kyu8/SumOfPositive.kt
@@ -1,11 +1,11 @@
 // https://www.codewars.com/kata/5715eaedb436cf5606000381
 /*
-You get an array of numbers, return the sum of all of the positives ones.
+You get an array of numbers, return the sum of all the positives ones.
 
 Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 Note: if there is nothing to sum, the sum is default to 0.
- */
+*/
 
 
 package Codewars.kyu8
@@ -13,7 +13,7 @@ package Codewars.kyu8
 object SumOfPositive {
 
     fun sum(numbers: IntArray): Int {
-        var result: Int = 0
+        var result = 0
 
         for (i in numbers) {
             if (i > 0) {
